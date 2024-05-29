8db73b52c9c7bc1e717d0b33eb2a2e6b9ceab76d
mka0123 <68675407+mka0123@users.noreply.github.com>
2024-05-30 00:46:36 +0800

    Update add_packages.sh

--- scripts/add_packages.sh
+++ scripts/add_packages.sh
@@ -17,7 +17,7 @@ EOL
 
 # {{ Add luci-app-poweroffdevice
 (cd friendlywrt/package && {
-    git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice.git --depth 1
+    git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git --depth 1
 })
 echo "CONFIG_PACKAGE_luci-app-poweroffdevice=y" >> configs/rockchip/01-nanopi
 # }}
