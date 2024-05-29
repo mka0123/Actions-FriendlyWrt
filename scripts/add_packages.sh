75438d60b7e2dd57d2e02468cfbfb300c7c2a15b
mka0123 <68675407+mka0123@users.noreply.github.com>
2024-05-29 23:36:21 +0800

    Update add_packages.sh

--- scripts/add_packages.sh
+++ scripts/add_packages.sh
@@ -20,5 +20,4 @@ EOL
     curl -fsSL  https://raw.githubusercontent.com/sirpdboy/other/master/patch/poweroff/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
     curl -fsSL  https://raw.githubusercontent.com/sirpdboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
 })
-EOL
 # }}
