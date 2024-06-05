16024da4976694388f79f3cfafa4054a3c92c232
mka0123 <68675407+mka0123@users.noreply.github.com>
2024-06-05 20:54:07 +0800

    Update add_packages.sh

--- scripts/add_packages.sh
+++ scripts/add_packages.sh
@@ -20,5 +20,4 @@ EOL
     git clone https://github.com/mka0123/luci-app-poweroff.git --depth 1
 })
 echo "CONFIG_PACKAGE_luci-app-poweroff=y" >> configs/rockchip/01-nanopi
-# echo "CONFIG_PACKAGE_luci-i18n-poweroff-zh-cn=y" >> configs/rockchip/01-nanopi
 # }}
